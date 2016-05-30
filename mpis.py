a8940d3461d600bb6156e5edc78d5098a84a4b79
Natanael Andrés Garrido <neoranger@openmailbox.org>
2016-05-30 15:33:26 +0000

    adding system tools functions

--- mpis.py
+++ mpis.py
@@ -109,7 +109,7 @@ def main():
 3) Development
 4) Internet
 5) Games
-6) Hacking Tools (Beta)
+6) System Tools
 7) Back
 8) Go Home
 					''')
@@ -468,7 +468,7 @@ def main():
 							else:
 								raw_input('Task Finished with errors. Press Enter to continue')
 						elif games == "3":
-							if os.system("sudo pacman snes9x-gtk") == 0:
+							if os.system("sudo pacman -S snes9x-gtk") == 0:
 								raw_input('Task Finished. Press Enter to continue')
 							else:
 								raw_input('Task Finished with errors. Press Enter to continue')
@@ -481,6 +481,64 @@ def main():
 							clear()
 							main_menu()
 						elif games == "gohome" or games == "6":
+							clear()
+							main_menu()
+						elif games == "exit":
+							sys.exit(0)
+						else:
+							print ("\033[1;31mSorry, invalid command!\033[1;m")
+							
+					while application_menu == "6":
+						clear()
+						print ('''
+1) Terminator
+2) Manjaro Settings Manager (GTK)
+3) Manjaro Settings Manager (Plasma5)
+4) VirtualBox
+5) Octopi (Pacman GUI)
+6) Pamac (Pacman GUI)
+7) Back
+8) Go Home
+						''')
+						systools = raw_input("\033[1;32mWhat you want to do?> \033[1;m")
+						if systools == "1":
+							if os.system("sudo pacman -S terminator") == 0:
+								raw_input('Task Finished. Press Enter to continue')
+							else:
+								raw_input('Task Finished with errors. Press Enter to continue')
+						elif systools == "2":
+							if os.system("sudo pacman -S manjaro-settings-manager") == 0:
+								raw_input('Task Finished. Press Enter to continue')
+							else:
+								raw_input('Task Finished with errors. Press Enter to continue')
+						elif systools == "3":
+							if os.system("sudo pacman -S manjaro-settings-manager") == 0:
+								os.system("sudo pacman -S manjaro-settings-manager-kcm")
+								os.system("sudo pacman -S manjaro-settings-manager-knotifier")
+								raw_input('Task Finished. Press Enter to continue')
+							else:
+								raw_input('Task Finished with errors. Press Enter to continue')
+						elif systools == "4":
+							if os.system("sudo pacman -S virtualbox") == 0:
+								print ("Don't forget install virtualbox's kernel modules")
+								raw_input('Task Finished. Press Enter to continue')
+							else:
+								raw_input('Task Finished with errors. Press Enter to continue')
+						elif systools == "5":
+							if os.system("sudo pacman -S octopi") == 0:
+								os.system("sudo pacman -S octopi-notifier")
+								raw_input('Task Finished. Press Enter to continue')
+							else:
+								raw_input('Task Finished with errors. Press Enter to continue')
+						elif systools == "6":
+							if os.system("sudo pacman pamac") == 0:
+								raw_input('Task Finished. Press Enter to continue')
+							else:
+								raw_input('Task Finished with errors. Press Enter to continue')
+						elif systools == "back" or systools == "7":
+							clear()
+							main_menu()
+						elif systools == "gohome" or systools == "8":
 							clear()
 							main_menu()
 						elif games == "exit":
