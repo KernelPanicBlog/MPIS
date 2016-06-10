40bbdce9eaa2ad77d33531058f19e871fad2f801
Federico Damián <federicodamians@gmail.com>
2016-06-10 09:57:48 -0300

    yaml: Add setup.py and move some things to make it work better

---
+++ wip/yaml/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# MPIS: Setup file
+#
+
+from distutils.core import setup
+
+def get_readme():
+    with open("README.md") as readme:
+        return readme.read()
+
+setup(name="mpis", version="unstable",
+      description="This script allows to configure the system, install some applications" \
+                  "for a regular work day thinked in developers, gamers, musicians and more...",
+      long_description=get_readme(),
+      keywords="manjaro post install script",
+      author="KernelPanicBlog Team",
+      url="https://kernelpanicblog.wordpress.com",
+      license="gplv3",
+      requires=["PyYAML"],
+      py_modules=["pympis.core"],
+      scripts=["mpis"])
