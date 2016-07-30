7428bc449d48d18c90a70a51a180c4a998edbcba
Federico Damián Schonborn <federicodamians@gmail.com>
2016-07-29 22:38:36 -0300

    Changes

--- PKGBUILD
+++ PKGBUILD
@@ -1,17 +1,18 @@
-pkgname="mpis-unstable-git"
-_pyname="mpis"
-_gitrepo="Script-Post-instalacion"
+pkgname="mpis-git"
+_gitrepo="MPIS"
 pkgver="0.2a"
 pkgrel="1"
-pkgdesc="This script allows to configure the system, install some applications for a regular work day thinked in developers, gamers, musicians and more... Unstable branch from Git."
-arch=('any')
+pkgdesc="This script allows to configure the system, install some applications for a regular work day thinked in developers, gamers, musicians and more..."
+arch=("any")
 url="https://kernelpanicblog.wordpress.com"
-license=('gplv3')
-makedepends=('python-setuptools')
-source=("git+https://github.com/KernelPanicBlog/Script-Post-instalacion.git#branch=unstable")
-md5sums=('SKIP')
+license=("GPLv3")
+depends=("python3")
+makedepends=("python-setuptools")
+options=(!emptydirs)
+source=("git+https://github.com/KernelPanicBlog/MPIS.git#branch=master")
+md5sums=("SKIP")
 
 package() {
-  cd "${srcdir}/${_gitrepo}"
-  python3 setup.py install --prefix=/usr --root="${pkgdir}"
+  cd "$srcdir/$_gitrepo"
+  python3 setup.py install --prefix=/usr --root="$pkgdir/" --optimize=1
 }
---
+++ mpislib/core.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+import os
+import subprocess
+from xml.etree.ElementTree import *
+
+messages = {
+    "msgMpis": "\n\033[1;32mOption > \033[1;m",
+    "msgTFWE": "Task finished with errors. Press Enter to continue...",
+    "msgTF": "Task finished. Press Enter to continue...",
+    "msgAur": "This application will be installed from the AUR "
+              "repository (community). It will be installed at "
+              "your own risk.",
+    "msgNf": "(not functional, yet)",
+    "msgCtrl+C": "\nYou had press the Ctrl+C keys combination. Accepted exit "
+                 "request. Bye!",
+    "msgNOp": "Sorry not valid option, Press any key to continue..."
+    }
+
+errors = {
+    "0x001": "command not found",
+    "0x002": "command exited with errors"
+    }
+
+
+def execute_command(command, option="False"):
+    flat = True
+    for cmd in command:
+        try:
+            cmd = cmd.split()
+            if cmd[0] in ["yaourt"]:
+                print(messages["msgAur"])
+            if flat and option == "True":
+                if subprocess.check_call(cmd) == 0:
+                    flat = True
+                else:
+                    flat = False
+                    break
+            elif flat or option != "True":
+                if subprocess.check_call(cmd) == 0:
+                    flat = True
+                else:
+                    flat = False
+        except subprocess.CalledProcessError:
+            print(errors["0x002"])
+    if flat:
+        pause(messages["msgTF"])
+    else:
+        pause(messages["msgTFWE"])
+
+
+def user_input():
+    try:
+        return input(messages["msgMpis"])
+    except ValueError:
+        return 0
+
+
+def pause(msg="Press any key to continue..."):
+    try:
+        a = str(input(msg))
+    except SyntaxError:
+        pass
+
+
+def mkopts(option):
+    upper = option.upper()
+    lower = option.lower()
+    lfirst = option[0].lower()
+    ufirst = option[0].upper()
+    tfirst = option.capitalize()
+    return [upper, lower, lfirst, ufirst, tfirst]
+
+
+def clear():
+    subprocess.run(["clear"])
+
+
+def sleep():
+    subprocess.run(["sleep", "2s"])
+
+
+def end_message(do_clear=True, shutdown=True, value=0):
+    if do_clear: clear()
+    print("""\033[1;36m
+Thanks for choosing us, we hope this script helped you.
+    - The KernelPanicBlog Team.
+Our web: https://kernelpanicblog.wordpress.com
+\033[1;m""")
+    if shutdown: sys.exit(value)
+
+
+def show_banner(do_clear=True):
+    if do_clear: clear()
+    print("""\033[1;36m
+ __  __ _____ _____  _____
+|  \/  |  __ \_   _|/ ____|
+| \  / | |__) || | | (___
+| |\/| |  ___/ | |  \___ \\
+| |  | | |    _| |_ ____) |
+|_|  |_|_|   |_____|_____/
+
+Manjaro Post Installation Script version 0.2a
+\033[1;m
+\033[1;32m
+Authors:
+    SniferL4bs  | https://www.sniferl4bs.com
+    NeoRanger   | https://www.neositelinux.com.ar
+    Harrinsoft  | harrinsoft@gmail.com
+Collaborative Blog: | https://kernelpanicblog.wordpress.com
+\033[1;m
+Script in testing phase, please report bugs :)
+https://github.com/KernelPanicBlog/MPIS/issues
+""")
+    pause()
+    clear()
+
+
+def show_help(do_clear=True):
+    if do_clear: clear()
+    print("""\033[1;36m
+Help:
+\033[1;m
+You can select an option with the given
+number or write 3 shortcuts:
+- back or b -> return to the previous option
+- help or h -> show help
+- exit or x or Ctrl+C -> finish the script execution
+""")
+    pause()
+    clear()
+
+
+def search_commands(name_app, l_apps):
+    for c in range(len(l_apps)):
+        if l_apps[c].name == name_app:
+            return l_apps[c]
+            break
+    return None
+
+
+def search_menu(name_menu, l_menus):
+    for c in range(len(l_menus)):
+        if l_menus[c].name == name_menu:
+            return c
+            break
+    return 0
+
+
+def not_repeated(name_cmd, l_apps):
+    for c in range(len(l_apps)):
+        if l_apps[c].name == name_cmd:
+            return True
+            break
+    return False
+
+
+class Item:
+    def __init__(self):
+        self.name = ""
+        self.title = ""
+        self.option = 0
+        self.ismenu = "False"
+
+
+class Menu:
+    def __init__(self):
+        self.name = ""
+        self.title = ""
+        self.items = []
+
+
+class App:
+    def __init__(self):
+        self.name = ""
+        self.sequentially = "False"
+        self.commands = []
+        self.messages = []
+
+
+class Mpis:
+    def __init__(self):
+        self.menus = []
+        self.apps = []
+        self.__load_config()
+
+    def __load_config(self):
+        root_dir = "/usr/lib/mpis/"
+        file_name = "menu.xml"
+        root_file = root_dir + file_name
+        menu_config = file_name if not os.path.exists(root_file) else root_file
+        with open(menu_config) as f:
+            uncode = f.read()
+            data = fromstring(uncode)
+            lst_menus = data.findall("menu")
+        for n_menu in lst_menus:
+            new_menu = Menu()
+            new_menu.name = n_menu.find("name").text
+            new_menu.title = n_menu.find("title").text
+            lst_items = []
+            items = n_menu.findall("items/item")
+            for item in items:
+                new_item = Item()
+                new_item.title = item.find("title").text
+                new_item.name = item.find("name").text
+                new_item.option = item.find("option").text
+                new_item.ismenu = item.get("ismenu")
+                commands = item.findall("commands")
+                if len(commands) != 0:
+                    if not not_repeated(item.find("name").text, self.apps):
+                        new_app = App()
+                        new_app.name = item.find("name").text
+                        new_app.sequentially = item.get("sequentially")
+                        lst_cmd = []
+                        for cmd in commands:
+                            all_cmd = cmd.findall("cmd")
+                            for c in all_cmd:
+                                lst_cmd.append(c.text)
+                        new_app.commands = lst_cmd
+                        self.apps.append(new_app)
+                lst_items.append(new_item)
+            new_menu.items = lst_items
+            self.menus.append(new_menu)
+
+    def reload(self):
+        self.__load_config()
